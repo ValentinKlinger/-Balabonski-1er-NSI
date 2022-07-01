73b4abfbf30e49a35c4cc69777907c087a3f3247
valentin <valentink.fr@gmail.com>
2022-07-01 16:57:02 +0200

    petites rectifications de programmes

--- algorithmes_de_tri/ex140p151.py
+++ algorithmes_de_tri/ex140p151.py
@@ -2,6 +2,7 @@
 # d'y chercher la plus petite valeur ni de l'échangé avec elle même. Modifier le programme 14 pour évité cette étape innutile.
 
 # programme 14 (il tris une liste par ordre croissant):
+'''
 def echange(t, i, j):
     tmp = t[i]
     t[i] = t[j]
@@ -13,14 +14,13 @@ def trie_par_selection(t):
         for j in range(i + 1,len(t)):
             if t[j] < t[m]:
                 m = j
-        echange(t, i, m)
+        echange(t, i, m)'''
 
 # mon obtimisation :
 def echange(t, i, j):
     tmp = t[i]
     t[i] = t[j]
     t[j] = tmp
-    print(t)
 
 def trie_par_selection(t):
     for i in range(len(t) - 1): # la modif est dans range(len(t)) qui devient range(len(t) - 1)
